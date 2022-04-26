a403369ce1b8b15c4ad85165ab62a46b67466206
Azen0n <jack.2013.ghost@gmail.com>
2022-04-26 13:08:47 +0500

    Back button is disabled at first pixel now, fix new pixels grid

--- static/js/binarization-visualization.js
+++ static/js/binarization-visualization.js
@@ -10,12 +10,17 @@ image.src = 'static/img/visualization-pixels.png';
 
 let container = document.getElementById('grid-container');
 let newContainer = document.getElementById('new-grid-container');
-let normalPixelPath = 'M 3 0 L 30 0 A 3 3 0 0 1 33 3 L 33 30 A 3 3 0 0 1 30 33 L 3 33 A 3 3 0 0 1 0 30 L 0 3 A 3 3 0 0 1 3 0';
 let largePixelPath = 'M 4 0 L 48 0 A 4 4 0 0 1 52 4 L 52 48 A 4 4 0 0 1 48 52 L 4 52 A 4 4 0 0 1 0 48 L 0 4 A 4 4 0 0 1 4 0';
 
 let colors = [];
 let pixels;
+let newPixels;
 let largePixel;
+
+let backBtn = document.getElementById('back');
+let forwardBtn = document.getElementById('forward');
+let i = 0;
+let j = 0;
 
 image.onload = () => {
     ctx.drawImage(image, 0, 0);
@@ -27,36 +32,53 @@ image.onload = () => {
         }
     }
 
-    function createPixels(size) {
-        let pixels = [];
-        for (let i = 0; i < size; ++i) {
-            pixels.push([]);
-            for (let j = 0; j < size; ++j) {
-                let element = document.createElement('div');
-                element.className = "grid-element";
-                element.style.backgroundColor = 'rgb(' + colors[i][j][0] + ', ' + colors[i][j][1] + ', ' + colors[i][j][2] + ')';
-                //let draw = SVG().width(33).height(33).addTo(element);
-                container.appendChild(element);
-                //draw.path(normalPixelPath).fill('rgb(' + colors[i][j][0] + ', ' + colors[i][j][1] + ', ' + colors[i][j][2] + ')');
-                pixels[i].push(element);
-            }
-        }
-        return pixels;
-    }
-
     pixels = createPixels(size);
+    newPixels = createNewPixels(size);
     largePixel = drawLargePixel();
-    selectPixel(0, 0);
-    calcBrightness(colors[j][i]);
+    selectPixel(i, j);
+    calcBrightness(colors[i][j]);
+    backBtn.disabled = true;
+}
+
+function createPixels(size) {
+    let pixels = [];
+    for (let i = 0; i < size; ++i) {
+        pixels.push([]);
+        for (let j = 0; j < size; ++j) {
+            let element = document.createElement('div');
+            element.className = "grid-element";
+            element.style.backgroundColor = 'rgb(' + colors[i][j][0] + ', ' + colors[i][j][1] + ', ' + colors[i][j][2] + ')';
+            container.appendChild(element);
+            pixels[i].push(element);
+        }
+    }
+    return pixels;
+}
+
+function createNewPixels(size) {
+    let pixels = [];
+    for (let i = 0; i < size; ++i) {
+        pixels.push([]);
+        for (let j = 0; j < size; ++j) {
+            let element = document.createElement('div');
+            element.className = "grid-element";
+            element.style.backgroundColor = 'rgb(255, 255, 255)';
+            newContainer.appendChild(element);
+            pixels[i].push(element);
+        }
+    }
+    return pixels;
 }
 
 function selectPixel(i, j) {
     pixels[i][j].classList.replace('grid-element', 'selected-grid-element');
+    newPixels[i][j].classList.replace('grid-element', 'selected-grid-element');
     changeLargePixelColor(colors[i][j]);
 }
 
 function turnDownPixel(i, j) {
     pixels[i][j].classList.replace('selected-grid-element', 'grid-element');
+    newPixels[i][j].classList.replace('selected-grid-element', 'grid-element');
 }
 
 function drawLargePixel() {
@@ -70,15 +92,8 @@ function drawLargePixel() {
 function changeLargePixelColor(color) {
     largePixel.fill('rgb(' + color[0] + ', ' + color[1] + ', ' + color[2] + ')');
     let colorCode = document.getElementById('color-code');
-    let RGB = '<b>R</b> ' + color[0] + '<br><b>G</b> ' + color[1] + '<br><b>B</b> ' + color[2];
-    //let hex = '<br><b>HEX</b> #' + color[0].toString(16) + color[1].toString(16) + color[2].toString(16);
-    colorCode.innerHTML = RGB;
+    colorCode.innerHTML = '<b>R</b> ' + color[0] + '<br><b>G</b> ' + color[1] + '<br><b>B</b> ' + color[2];
 }
-
-let backBtn = document.getElementById('back');
-let forwardBtn = document.getElementById('forward');
-let i = 0;
-let j = 0;
 
 function nextIndex(i, j) {
     let nextI = i, nextJ = j;
@@ -117,6 +132,7 @@ forwardBtn.addEventListener('click', () => {
     [i, j] = nextIndex(i, j);
     selectPixel(i, j);
     calcBrightness(colors[i][j]);
+    backBtn.disabled = i === 0 && j === 0;
 });
 
 backBtn.addEventListener('click', () => {
@@ -124,6 +140,7 @@ backBtn.addEventListener('click', () => {
     [i, j] = prevIndex(i, j);
     selectPixel(i, j);
     calcBrightness(colors[i][j]);
+    backBtn.disabled = i === 0 && j === 0;
 });
 
 function calcBrightness(color) {
@@ -156,47 +173,17 @@ function calcBrightness(color) {
 
     if (brightness >= 128) {
         instructionText.innerHTML += brightness + ' ≥ 128 → 255';
-        if (isTimeToUpdate()) {
-            appendProcessedPixel([255, 255, 255]);
-        }
+        changeNewPixelColor([255, 255, 255]);
     } else {
         instructionText.innerHTML += brightness + ' < 128 → 0';
-        if (isTimeToUpdate()) {
-            appendProcessedPixel([0, 0, 0]);
-        }
+        changeNewPixelColor([0, 0, 0]);
     }
     instructions.append(document.createElement('div'), instructionText);
 }
 
-let newPixels = [];
-for (let i = 0; i < size; ++i) {
-    newPixels.push([]);
-    for (let j = 0; j < size; ++j) {
-        newPixels[i].push(false);
-    }
-}
-let lastPixelIndex = [0, -1];
-
-function isTimeToUpdate() {
-    if (lastPixelIndex[0] === 4 && lastPixelIndex[1] === 4) {
-        return false;
-    }
-    let nextPixelIndex = nextIndex(...lastPixelIndex);
-    if (nextPixelIndex[0] === i && nextPixelIndex[1] === j) {
-        lastPixelIndex = nextPixelIndex;
-        return true;
-    }
-    return false;
-}
-
-function appendProcessedPixel(color) {
-    let element = document.createElement('div');
-    element.classList.add('grid-element');
+function changeNewPixelColor(color) {
     if (color[0] + color[1] + color[2] === 255 * 3) {
-        element.classList.add('bright');
+        newPixels[i][j].classList.add('bright');
     }
-    let draw = SVG().width(33).height(33).addTo(element);
-    newContainer.appendChild(element);
-    draw.path(normalPixelPath).fill('rgb(' + color[0] + ', ' + color[1] + ', ' + color[2] + ')');
-    newPixels[i][j] = true;
+    newPixels[i][j].style.backgroundColor = 'rgb(' + color[0] + ', ' + color[1] + ', ' + color[2] + ')';
 }
