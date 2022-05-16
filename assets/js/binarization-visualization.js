8974e0020f4a7ed17d305a7fd64e647d3cf34a1d
Azen0n <jack.2013.ghost@gmail.com>
2022-05-16 23:28:53 +0500

    Refactor binarization visualization

--- assets/js/binarization-visualization.js
+++ assets/js/binarization-visualization.js
@@ -1,191 +1,196 @@
-let size = 5;
+class Visualizer {
+    constructor(size, image) {
+        this.size = size;
+        this.image = image;
+        this.backBtn = document.getElementById('back');
+        this.forwardBtn = document.getElementById('forward');
 
-let canvas = document.createElement('canvas');
-canvas.width = size;
-canvas.height = size;
-const ctx = canvas.getContext('2d');
+        this.row = 0;
+        this.column = 0;
+        this.largePixel = document.getElementById('large-element');
+
+        this.image.onload = () => {
+            this.colors = this.getColors();
+            this.pixels = this.fillPixelGrid();
+            this.processedPixels = this.fillProcessedPixelGrid();
+            this.togglePixel(this.row, this.column);
+            this.updateInstructions();
+            this.backBtn.disabled = true;
+        }
+
+        this.forwardBtn.addEventListener('click', () => {
+            this.switchPixel(...this.nextPixel());
+        });
+
+        this.backBtn.addEventListener('click', () => {
+            this.switchPixel(...this.prevPixel());
+        });
+    }
+
+    getColors() {
+        let canvas = document.createElement('canvas');
+        canvas.width = this.size;
+        canvas.height = this.size;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(this.image, 0, 0, this.size, this.size);
+
+        let colors = [];
+        for (let i = 0; i < this.size; ++i) {
+            colors.push([]);
+            for (let j = 0; j < this.size; ++j) {
+                const pixelData = ctx.getImageData(i, j, 1, 1).data;
+                colors[i].push([pixelData[0], pixelData[1], pixelData[2]]);
+            }
+        }
+        return colors;
+    }
+
+    fillPixelGrid() {
+        let pixelGrid = document.getElementById('grid-container');
+        let pixels = [];
+        for (let i = 0; i < this.size; ++i) {
+            pixels.push([]);
+            for (let j = 0; j < this.size; ++j) {
+                let pixel = document.createElement('div');
+                pixel.className = 'grid-element';
+                pixel.style.backgroundColor = this.RGBAsString(this.colors[i][j]);
+                pixel.addEventListener('click', () => {
+                    this.switchPixel(i, j);
+                });
+                pixelGrid.appendChild(pixel);
+                pixels[i].push(pixel);
+            }
+        }
+        return pixels;
+    }
+
+    RGBAsString(color) {
+        return `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+    }
+
+    fillProcessedPixelGrid() {
+        let pixelGrid = document.getElementById('new-grid-container');
+        let pixels = [];
+        for (let i = 0; i < this.size; ++i) {
+            pixels.push([]);
+            for (let j = 0; j < this.size; ++j) {
+                let pixel = document.createElement('div');
+                pixel.className = "grid-element";
+                pixel.style.backgroundColor = 'rgb(255, 255, 255)';
+                pixelGrid.appendChild(pixel);
+                pixels[i].push(pixel);
+            }
+        }
+        return pixels;
+    }
+
+    switchPixel(row, column) {
+        this.togglePixel(this.row, this.column);
+        [this.row, this.column] = [row, column];
+        this.togglePixel(this.row, this.column);
+        this.changeLargePixel(this.colors[this.row][this.column]);
+        this.updateInstructions();
+
+        this.backBtn.disabled = this.row === 0 && this.column === 0;
+    }
+
+    togglePixel(row, column) {
+        let pixel = this.pixels[row][column];
+        let processedPixel = this.processedPixels[row][column];
+
+        if (pixel.classList.contains('selected-grid-element')) {
+            pixel.classList.replace('selected-grid-element', 'grid-element');
+            processedPixel.classList.replace('selected-grid-element', 'grid-element');
+        } else {
+            pixel.classList.replace('grid-element', 'selected-grid-element');
+            processedPixel.classList.replace('selected-grid-element', 'grid-element');
+        }
+    }
+
+    changeLargePixel(color) {
+        this.largePixel.style.backgroundColor = 'rgb(' + color[0] + ', ' + color[1] + ', ' + color[2] + ')';
+        let colorCode = document.getElementById('color-code');
+        colorCode.innerHTML =
+            '<div class="color-red"><b>R</b> ' + color[0] + '</div>' +
+            '<div class="color-green"><b>G</b> ' + color[1] + '</div>' +
+            '<div class="color-blue"><b>B</b> ' + color[2] + '</div>';
+    }
+
+    updateInstructions() {
+        let instructions = document.getElementById('instructions');
+        instructions.innerHTML = '';
+
+        let color = this.colors[this.row][this.column];
+        let threshold = 128;
+        let brightness = Math.round(0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2]);
+
+        if (brightness >= threshold) {
+            this.changeProcessedPixelColor([255, 255, 255]);
+        } else {
+            this.changeProcessedPixelColor([0, 0, 0]);
+        }
+
+        let instructionHeaders = [
+            'Вычисление яркости пикселя',
+            `Сравнение яркости с пороговым значением&nbsp;<span class="mathjax-font">T = ${threshold}</span>`
+        ];
+
+        let instructionDescriptions = [
+            'brightness = ' +
+            `0.299 ⋅ <span class="color-red">${color[0]}</span> + ` +
+            `0.587 ⋅ <span class="color-green">${color[1]}</span> + ` +
+            `0.114 ⋅ <span class="color-blue">${color[2]}</span> = ${brightness}`,
+            brightness >= threshold ? `${brightness} ≥ ${threshold} → 255` : `${brightness} < ${threshold} → 0`
+        ];
+
+        for (let i = 0; i < instructionHeaders.length; ++i) {
+            let instruction =
+                `<div class="instruction-number">${i + 1}</div>` +
+                `<div class="instruction">${instructionHeaders[i]}</div>` +
+                `<div></div>` +
+                `<div class="instruction-formula mathjax-font">${instructionDescriptions[i]}</div>`;
+
+            instructions.innerHTML += instruction;
+        }
+    }
+
+    changeProcessedPixelColor(color) {
+        let pixel = this.processedPixels[this.row][this.column];
+        if (mean(color) > 245) {
+            pixel.classList.add('bright');
+        }
+        pixel.style.backgroundColor = this.RGBAsString(color);
+    }
+
+    nextPixel() {
+        let row = this.row, column = this.column;
+        if (++column >= this.size) {
+            column = 0;
+            if (++row >= this.size) {
+                row = 0;
+            }
+        }
+        return [row, column];
+    }
+
+    prevPixel() {
+        let row = this.row, column = this.column;
+        if (--column < 0) {
+            column = this.size - 1;
+            if (--row < 0) {
+                row = this.size - 1;
+            }
+        }
+        return [row, column];
+    }
+}
+
+function mean(array) {
+    let sum = array.reduce((sum, a) => sum + a, 0);
+    return sum / array.length;
+}
 
 let image = new Image();
 image.src = '../static/img/visualization-pixels.png';
 
-let container = document.getElementById('grid-container');
-let newContainer = document.getElementById('new-grid-container');
-
-let colors = [];
-let pixels;
-let newPixels;
-let largePixel = document.getElementById('large-element');
-
-let backBtn = document.getElementById('back');
-let forwardBtn = document.getElementById('forward');
-let currentRow = 0;
-let currentColumn = 0;
-
-image.onload = () => {
-    ctx.drawImage(image, 0, 0);
-    for (let i = 0; i < size; ++i) {
-        colors.push([]);
-        for (let j = 0; j < size; ++j) {
-            const pixelData = ctx.getImageData(i, j, 1, 1).data;
-            colors[i].push([pixelData[0], pixelData[1], pixelData[2]]);
-        }
-    }
-
-    pixels = createPixels(size);
-    newPixels = createNewPixels(size);
-    selectPixel(currentRow, currentColumn);
-    calcBrightness(colors[currentRow][currentColumn]);
-    backBtn.disabled = true;
-}
-
-function createPixels(size) {
-    let pixels = [];
-    for (let i = 0; i < size; ++i) {
-        pixels.push([]);
-        for (let j = 0; j < size; ++j) {
-            let element = document.createElement('div');
-            element.className = "grid-element";
-            element.style.backgroundColor = 'rgb(' + colors[i][j][0] + ', ' + colors[i][j][1] + ', ' + colors[i][j][2] + ')';
-            container.appendChild(element);
-            element.addEventListener('click', function () {
-                turnDownPixel(currentRow, currentColumn);
-                [currentRow, currentColumn] = [i, j];
-                selectPixel(currentRow, currentColumn);
-                calcBrightness(colors[currentRow][currentColumn]);
-                backBtn.disabled = currentRow === 0 && currentColumn === 0;
-            });
-            pixels[i].push(element);
-        }
-    }
-    return pixels;
-}
-
-function createNewPixels(size) {
-    let pixels = [];
-    for (let i = 0; i < size; ++i) {
-        pixels.push([]);
-        for (let j = 0; j < size; ++j) {
-            let element = document.createElement('div');
-            element.className = "grid-element";
-            element.style.backgroundColor = 'rgb(255, 255, 255)';
-            newContainer.appendChild(element);
-            pixels[i].push(element);
-        }
-    }
-    return pixels;
-}
-
-function selectPixel(i, j) {
-    pixels[i][j].classList.replace('grid-element', 'selected-grid-element');
-    newPixels[i][j].classList.replace('grid-element', 'selected-grid-element');
-    changeLargePixelColor(colors[i][j]);
-}
-
-function turnDownPixel(i, j) {
-    pixels[i][j].classList.replace('selected-grid-element', 'grid-element');
-    newPixels[i][j].classList.replace('selected-grid-element', 'grid-element');
-}
-
-function changeLargePixelColor(color) {
-    largePixel.style.backgroundColor = 'rgb(' + color[0] + ', ' + color[1] + ', ' + color[2] + ')';
-    let colorCode = document.getElementById('color-code');
-    colorCode.innerHTML =
-        '<div class="color-red"><b>R</b> ' + color[0] + '</div>' +
-        '<div class="color-green"><b>G</b> ' + color[1] + '</div>' +
-        '<div class="color-blue"><b>B</b> ' + color[2] + '</div>';
-}
-
-function nextIndex(i, j) {
-    let nextI = i, nextJ = j;
-    if (j + 1 < size) {
-        ++nextJ;
-    } else {
-        if (i + 1 < size) {
-            ++nextI;
-            nextJ = 0;
-        } else {
-            nextI = 0;
-            nextJ = 0;
-        }
-    }
-    return [nextI, nextJ];
-}
-
-function prevIndex(i, j) {
-    let prevI = i, prevJ = j;
-    if (j - 1 >= 0) {
-        --prevJ;
-    } else {
-        if (i - 1 >= 0) {
-            --prevI;
-            prevJ = size - 1;
-        } else {
-            prevI = size - 1;
-            prevJ = size - 1;
-        }
-    }
-    return [prevI, prevJ];
-}
-
-forwardBtn.addEventListener('click', () => {
-    turnDownPixel(currentRow, currentColumn);
-    [currentRow, currentColumn] = nextIndex(currentRow, currentColumn);
-    selectPixel(currentRow, currentColumn);
-    calcBrightness(colors[currentRow][currentColumn]);
-    backBtn.disabled = currentRow === 0 && currentColumn === 0;
-});
-
-backBtn.addEventListener('click', () => {
-    turnDownPixel(currentRow, currentColumn);
-    [currentRow, currentColumn] = prevIndex(currentRow, currentColumn);
-    selectPixel(currentRow, currentColumn);
-    calcBrightness(colors[currentRow][currentColumn]);
-    backBtn.disabled = currentRow === 0 && currentColumn === 0;
-});
-
-function calcBrightness(color) {
-    let instructions = document.getElementById('instructions');
-    instructions.innerHTML = '';
-    let number = document.createElement('div');
-    number.className = 'instruction-number';
-    number.innerHTML = '1';
-    let instruction = document.createElement('div');
-    instruction.className = 'instruction';
-    instruction.innerHTML = 'Вычисление яркости пикселя';
-    instructions.append(number, instruction);
-    let brightness = Math.round(0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2]);
-    let instructionText = document.createElement('div');
-    instructionText.classList.add('mathjax-font', 'instruction-formula');
-    instructionText.innerHTML = 'brightness = ' +
-        '0.299 ⋅ <span class="color-red">' + color[0] + '</span> + ' +
-        '0.587 ⋅ <span class="color-green">' + color[1] + '</span> + ' +
-        '0.114 ⋅ <span class="color-blue">' + color[2] + '</span> = ' + brightness;
-    instructions.append(document.createElement('div'), instructionText);
-
-    number = document.createElement('div');
-    number.className = 'instruction-number';
-    number.innerHTML = '2';
-    instruction = document.createElement('div');
-    instruction.className = 'instruction';
-    instruction.innerHTML = 'Сравнение яркости с пороговым значением&nbsp;<span class="mathjax-font">T = 128</span>';
-    instructions.append(number, instruction);
-
-    instructionText = document.createElement('div');
-    instructionText.classList.add('mathjax-font', 'instruction-formula');
-
-    if (brightness >= 128) {
-        instructionText.innerHTML += brightness + ' ≥ 128 → 255';
-        changeNewPixelColor([255, 255, 255]);
-    } else {
-        instructionText.innerHTML += brightness + ' < 128 → 0';
-        changeNewPixelColor([0, 0, 0]);
-    }
-    instructions.append(document.createElement('div'), instructionText);
-}
-
-function changeNewPixelColor(color) {
-    if (color[0] + color[1] + color[2] === 255 * 3) {
-        newPixels[currentRow][currentColumn].classList.add('bright');
-    }
-    newPixels[currentRow][currentColumn].style.backgroundColor = 'rgb(' + color[0] + ', ' + color[1] + ', ' + color[2] + ')';
-}
+const visualizer = new Visualizer(5, image);
