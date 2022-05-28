c28e09916c3b752cd440ead54db7bfab5960c35f
Azen0n <jack.2013.ghost@gmail.com>
2022-05-28 20:54:56 +0500

    Minor changes

--- base/urls.py
+++ base/urls.py
@@ -4,7 +4,7 @@ from base import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('process_image/', views.process_image, name='process_image'),
+    path('articles/process_image/', views.process_image, name='process_image'),
     path('articles/<int:id>', views.algorithm, name='algorithm'),
     path('articles/<int:id>/tests', views.tests, name='tests'),
     path('articles/<int:article_id>/tests/<int:test_id>', views.test, name='test'),
